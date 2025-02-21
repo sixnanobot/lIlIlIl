7bb15812c0fec1592c1509604f8a52a096009aa0
sixnanobot <188917475+sixnanobot@users.noreply.github.com>
2025-02-22 00:15:28 +0100

    Update sound.ps1

--- sound.ps1
+++ sound.ps1
@@ -1,15 +1,4 @@
-$mp3Url = "https://audio.jukehost.co.uk/PZU15MPUQ6cXQDSIawANs6AzMgRPy58j"
-$mp3Path = "$env:Temp\temp.mp3"
-Invoke-WebRequest -Uri $mp3Url -OutFile $mp3Path
-Add-Type -TypeDefinition @"
-using System;
-using System.Media;
-public class SoundPlayer {
-    public void PlaySound(string file) {
-        var player = new System.Media.SoundPlayer(file);
-        player.PlaySync();
-    }
-}
-"@
-$player = New-Object SoundPlayer
-$player.PlaySound($mp3Path)
+$path = "a.wav"
+iwr -Uri "https://audio.jukehost.co.uk/gRKoaYb8aW7TTgJ2C1S3jp5g2WTZlMGF" -OutFile $path
+$soundplayer = New-Object Media.SoundPlayer $path
+$soundplayer.PlaySync()
